Fixed scaling issues, added tutorial and save functionality
Fixed issues with segments not scaling correctly, still need to fix when rescaling the window.
Added a tutorial window.
Added save functionality updates with amg.
Added goal overlay and (very expensive) color overlay
removed useless modifiers, made most things 'user study proof'

src/view/mainwindow.py
@@ -4,14 +4,17 @@ from PyQt5.QtGui import *
 import logging
 from skimage.measure import regionprops
 import numpy as np
+import matplotlib.pyplot as plt
 
 from constants import *
 from modifiers import modifiers
 
 from .viewport import Viewport
 from .parameter_tab import ParameterTab
-from model.segmentation import segment
+from model.segmentation import segment, apply_rag
 from .timeline import Timeline
+from .tutorial import Tutorial
+from model.AMG import AMG, Node as AMGNode
 
 class MainWindow(QMainWindow):
     """
@@ -27,7 +30,13 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("ThinSight")
         self.setWindowIcon(QIcon(str(PROJECT_DIRECTORY / "resources" / "images" / "icon.png")))
         self._setup_ui()
-        self.viewport.load_image("C:/Users/Drasath/Desktop/Belangrijke Bestanden/Uni/Bachelor/Year 3/Bachelor Project/thin-section-segmentation/datasets/test.tif")
+
+        self.save_path = None
+        self.amg = AMG(self)
+        
+        self.save_path = Path(PROJECT_DIRECTORY / "saves" / f"subject{SUBJECT_NR}.save")
+        self.viewport.load_image(str(PROJECT_DIRECTORY / "datasets" / "example.tif"))
+        # self.global_segmentation()
 
     def _setup_ui(self):
 
@@ -36,10 +45,11 @@ class MainWindow(QMainWindow):
             "File": [
                 {"Open": self.open_file},
                 {"Save": self.save_file},
-                {"Preferences": lambda: None},
-                {"Recent Files": lambda: None},
-                {"Recent Projects": lambda: None},
-                {"Export": lambda: None},
+                {"Save As": self.save_as_file},
+                # {"Preferences": lambda: None},
+                # {"Recent Files": lambda: None},
+                # {"Recent Projects": lambda: None},
+                # {"Export": lambda: None},
                 {"Exit": self.close}
             ],
             "Edit": [
@@ -49,12 +59,16 @@ class MainWindow(QMainWindow):
             ],
             "View": [
                 {"Show Borders": self.toggle_borders},
-                {"Show RAG": self.toggle_rag},
+                {"Show Segments": self.toggle_colors},
+                {"Show Goal": self.toggle_overlay},
+                # {"Show RAG": self.toggle_rag},
                 {"Reset View": self.reset_view}
             ],
             "Analysis": [
                 {"Show Clustering": self.show_clustering},
-                {"Jaccard Distance": self.jaccard_distance}
+                {"Show Histogram": lambda: self.show_histogram()},
+                {"Jaccard Distance": self.jaccard_distance},
+                {"Print AMG": lambda: print(self.amg)}
             ],
             "Segmentation": [
                 # {"Refine": },
@@ -71,6 +85,7 @@ class MainWindow(QMainWindow):
             for action in menu_actions:
                 (action_name, action_function), = action.items()
                 menu.addAction(action_name, action_function)
+
         #!SECTION
 
         # SECTION - Central Widget
@@ -101,10 +116,52 @@ class MainWindow(QMainWindow):
         self.outliner.itemClicked.connect(self.select_region)
         # sidebar_layout.addWidget(self.outliner)
 
-        sidebar_layout.addWidget(QLabel("Tutorial"))
+        sidebar_layout.addWidget(Tutorial(self.save_as_file))
 
         self.inspector = QTabWidget()
         sidebar_layout.addWidget(self.inspector)
+
+        self.global_parameters = QWidget()
+        global_parameters_layout = QVBoxLayout()
+        global_parameters_layout.setAlignment(Qt.AlignTop)
+        self.global_parameters.setLayout(global_parameters_layout)
+        
+        self.global_parameters.n_segments = QLineEdit()
+        self.global_parameters.compactness = QLineEdit()
+        self.global_parameters.min_lum = QLineEdit()
+        self.global_parameters.min_size = QLineEdit()
+        self.global_parameters.quality = QLineEdit()
+
+        self.global_parameters.n_segments.setValidator(QIntValidator(1, 2000))
+        self.global_parameters.compactness.setValidator(QDoubleValidator(0, 1, 2))
+        self.global_parameters.min_lum.setValidator(QDoubleValidator(0, 1, 2))
+        self.global_parameters.min_size.setValidator(QIntValidator(1, 10000))
+        self.global_parameters.quality.setValidator(QDoubleValidator(0, 1, 2))
+        
+        global_parameters_layout.addWidget(QLabel("Number of Segments (0-2000)"))
+        global_parameters_layout.addWidget(self.global_parameters.n_segments)
+        global_parameters_layout.addWidget(QLabel("Compactness (0.0-1.0)"))
+        global_parameters_layout.addWidget(self.global_parameters.compactness)
+        global_parameters_layout.addWidget(QLabel("Minimum Luminance (0.0-1.0)"))
+        global_parameters_layout.addWidget(self.global_parameters.min_lum)
+        global_parameters_layout.addWidget(QLabel("Minimum Grain Size (0-10000)"))
+        global_parameters_layout.addWidget(self.global_parameters.min_size)
+        global_parameters_layout.addWidget(QLabel("Quality (0.0-1.0)"))
+        global_parameters_layout.addWidget(self.global_parameters.quality)
+        
+        global_parameters_layout.addWidget(QPushButton("Segment", clicked=self.global_segmentation))
+        self.inspector.addTab(self.global_parameters, "Global Parameters")
+
+        self.rag_parameters = QWidget()
+        rag_parameters_layout = QVBoxLayout()
+        rag_parameters_layout.setAlignment(Qt.AlignTop)
+        self.rag_parameters.setLayout(rag_parameters_layout)
+        self.rag_parameters.threshold = QLineEdit()
+        self.rag_parameters.threshold.setValidator(QDoubleValidator(0, 1, 2))
+        rag_parameters_layout.addWidget(QLabel("Threshold (0.0-1.0)"))
+        rag_parameters_layout.addWidget(self.rag_parameters.threshold)
+        self.rag_parameters.layout().addWidget(QPushButton("Apply RAG", clicked=self.apply_rag))
+        self.inspector.addTab(self.rag_parameters, "RAG Parameters")
 
         self.refinement = QWidget()
         refinement_layout = QVBoxLayout()
@@ -122,22 +179,12 @@ class MainWindow(QMainWindow):
 
         self.inspector.addTab(self.refinement, "Refinement")
 
-        self.global_parameters = QWidget()
-        global_parameters_layout = QVBoxLayout()
-        self.global_parameters.setLayout(global_parameters_layout)
-        global_parameters_layout.addWidget(QLabel("Global Parameters"))
-        global_parameters_layout.addWidget(QPushButton("Segment", clicked=self.global_segmentation))
-        self.inspector.addTab(self.global_parameters, "Global Parameters")
+        # self.properties = QWidget()
+        # properties_layout = QVBoxLayout()
+        # self.properties.setLayout(properties_layout)
+        # properties_layout.addWidget(QLabel("Properties"))
 
-        self.properties = QWidget()
-        properties_layout = QVBoxLayout()
-        self.properties.setLayout(properties_layout)
-        properties_layout.addWidget(QLabel("Properties"))
-
-        self.inspector.addTab(self.properties, "Properties")
-
-        # for modifier in modifiers:
-        #     self.inspector.addTab(ParameterTab(self, modifier), modifier.name)
+        # self.inspector.addTab(self.properties, "Properties")
 
         #!SECTION
 
@@ -148,19 +195,59 @@ class MainWindow(QMainWindow):
     def open_file(self):
         (file_path, _) = QFileDialog.getOpenFileName(filter="Images (*.tif *.tiff)", directory=str(PROJECT_DIRECTORY / "datasets")) # TODO - Allow opening of save files. LH 
         if file_path:
-            logging.info(f"File opened: {file_path}")
-            self.viewport.load_image(file_path)
+            if file_path.endswith(".tif") or file_path.endswith(".tiff"):
+                logging.info(f"File opened: {file_path}")
+                self.viewport.load_image(file_path)
+            elif file_path.endswith(SAVE_FILE_EXTENTION):
+                logging.info(f"Segments opened: {file_path}")
+                segments = np.load(file_path)
+                self.viewport.set_segments(segments)
+            
         else:
             logging.info("No file selected.")
 
+    def load_file(self, cache=None):
+        path = self.save_path
+        if cache is not None:
+            path = str(path.parent / (str(cache) + "_" + path.name))
+
+        if path:
+            logging.info("Loading file...")
+            with open(path, 'rb') as file:
+                self.viewport.segments = np.load(file)
+
+    def store_file(self, cache=None):
+        path = self.save_path
+        if cache is not None:
+            path = str(path.parent / (str(cache) + "_" + path.name))
+            
+        with open(path, 'wb') as file:
+            np.save(file, self.viewport.segments)
+
     def save_file(self):
-        logging.info("Saving file...")
+        if self.save_path:
+            logging.info("Saving file...")
+            self.store_file()
+        else:
+            self.save_as_file()
+
+    def save_as_file(self):
+        (file_path, _) = QFileDialog.getSaveFileName(filter="Save Files (*.save)", directory=str(PROJECT_DIRECTORY / "saves"))
+        if file_path:
+            logging.info(f"File saved as: {file_path}")
+            if file_path.endswith(SAVE_FILE_EXTENTION):
+                self.save_path = file_path
+                self.save_file()
+        else:
+            logging.info("No file location selected.")
 
     def undo(self):
-        pass
+        self.amg.undo()
+        self.load_file(self.amg.activeNode.index)
 
     def redo(self):
-        pass
+        self.amg.redo()
+        self.load_file(self.amg.activeNode.index)
 
     def toggle_borders(self):
         self.viewport.toggle_borders()
@@ -172,6 +259,14 @@ class MainWindow(QMainWindow):
         pass
 
     def global_segmentation(self):
+        parameters = {
+            "n_segments": int(self.global_parameters.n_segments.text() or 800),
+            "compactness": float(self.global_parameters.compactness.text() or 0.1),
+            "min_size": int(self.global_parameters.min_size.text() or 500),
+            "min_lum": float(self.global_parameters.min_lum.text() or 0.2),
+            "quality": float(self.global_parameters.quality.text() or 0.5)
+        }
+        self.amg.addNode(AMGNode({"modifier": "Global segmentation", "parameters": []}))
         # open segments cache to avoid recomputing
         # cache = np.load("segments.npy")
         # if cache is not None:
@@ -182,7 +277,7 @@ class MainWindow(QMainWindow):
         #         self.outliner.addItem(f"Region {prop.label}")
         #     return
 
-        segments, lc = segment(self.viewport.image)
+        segments, lc = segment(self.viewport.image, **parameters)
         segments += 1
         self.viewport.set_segments(segments)
         props = regionprops(segments)
@@ -200,6 +295,9 @@ class MainWindow(QMainWindow):
         modifier = modifiers[self.refinement.segmentation_method.currentIndex()]
         parameters = self.refinement.parameters.get_parameters()
         
+        self.amg.addNode(AMGNode({"modifier": modifier.name, "parameters": parameters}))
+
+        qApp.changeOverrideCursor(Qt.BusyCursor)
         if modifier.type == "image":
             result = modifier.apply(self.viewport.image, self.viewport.segments, parameters)
             self.viewport.set_image(result)
@@ -214,8 +312,10 @@ class MainWindow(QMainWindow):
                 result = modifier.apply(self.viewport.image, mask, parameters)
                 segments[result] = segment
                 self.viewport.set_segments(segments)
+        qApp.restoreOverrideCursor()
 
     def merge_regions(self):
+        self.amg.addNode(AMGNode({"modifier": "Merge regions", "parameters": self.viewport.selected_segments}))
         segments = self.viewport.segments.copy()
         for segment in self.viewport.selected_segments:
             segments[segments == segment] = self.viewport.selected_segments[0]
@@ -256,3 +356,16 @@ class MainWindow(QMainWindow):
 
     def reset_view(self):
         self.viewport.reset_transform()
+
+    def show_histogram(self):
+        plt.hist(self.viewport.image.flatten(), bins=256, range=(0, 256), density=True)
+        plt.show()
+
+    def apply_rag(self):
+        self.viewport.set_segments(apply_rag(self.viewport.image, self.viewport.segments, float(self.rag_parameters.threshold.text()) or 0.08))
+
+    def toggle_colors(self):
+        self.viewport.toggle_colors()
+
+    def toggle_overlay(self):
+        self.viewport.toggle_overlay()

src/view/viewport.py
@@ -1,4 +1,3 @@
-from PyQt5.QtGui import QWheelEvent
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -6,6 +5,7 @@ import numpy as np
 from skimage import io, color, segmentation, graph
 import logging
 
+from constants import *
 class Viewport(QWidget):
     """
     """
@@ -16,6 +16,10 @@ class Viewport(QWidget):
         size = 1200
         self.setMinimumSize(size, size)
 
+        self.reset()
+
+    def reset(self):
+        size = 1200
         self.transform = QTransform()
         self.image: np.ndarray = None
         self.q_image = QImage(size, size, QImage.Format_RGB888)
@@ -24,8 +28,11 @@ class Viewport(QWidget):
         self.rag: graph.LineCollection = None
         self.show_borders: bool = False
         self.show_rag: bool = False
+        self.show_colors: bool = False
+        self.show_overlay: bool = False
         self.resize_scale: float = 1
         self.mouse_moved = False
+        self.zoom_level = 1
         self.selected_segments = []
 
     def mousePressEvent(self, event):
@@ -37,10 +44,10 @@ class Viewport(QWidget):
         if event.buttons() == Qt.LeftButton:
             qApp.changeOverrideCursor(Qt.ClosedHandCursor)
             self.mouse_moved = True
-            self.transform.translate((event.x() - self.last_pos.x()) / self.resize_scale, (event.y() - self.last_pos.y()) / self.resize_scale)
+            dx, dy = (event.x() - self.last_pos.x()) / self.resize_scale, (event.y() - self.last_pos.y()) / self.resize_scale
+            self.transform.translate(dx, dy)
             self.update()
             self.last_pos = event.pos()
-            # TODO - Add constraints to the movement. LH
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -53,8 +60,8 @@ class Viewport(QWidget):
                 # Select region or add marker depending on mode
                 if self.segments is None:
                     return
-                mouse_pos = QPoint(event.x(), event.y())
-                mouse_pos = self.transform.inverted()[0].scale(self.resize_scale, self.resize_scale).map(mouse_pos)
+                mouse_pos = self.transform.inverted()[0].map(event.pos()) * self.resize_scale
+                logging.info(f"Mouse position: {event.pos()} {mouse_pos} {self.width()} {self.height()}")
                 if mouse_pos.x() < 0 or mouse_pos.x() >= self.segments.shape[1] or mouse_pos.y() < 0 or mouse_pos.y() >= self.segments.shape[0]:
                     return
 
@@ -68,19 +75,19 @@ class Viewport(QWidget):
         # TODO - Zoom towards mouse position. LH
         # TODO - Zoom away from mouse position, and towards the center. LH
         if event.angleDelta().y() > 0:
-            if self.resize_scale > 500:
+            if self.zoom_level > 500:
                 return
             self.transform.translate(self.width() / 2, self.height() / 2)
             self.transform.scale(1.1, 1.1)   
             self.transform.translate(-self.width() / 2, -self.height() / 2)
-            self.resize_scale *= 1.1
+            self.zoom_level *= 1.1
         else:
-            if self.resize_scale < .1:
+            if self.zoom_level < .1:
                 return
             self.transform.translate(self.width() / 2, self.height() / 2)
             self.transform.scale(0.9, 0.9)   
             self.transform.translate(-self.width() / 2, -self.height() / 2)
-            self.resize_scale *= 0.9
+            self.zoom_level *= 0.9
         self.update()
 
     def paintEvent(self, event):
@@ -88,10 +95,16 @@ class Viewport(QWidget):
         qp.setTransform(self.transform)
         rect = event.rect()
 
-        self._draw_borders(qp) if self.show_borders else qp.drawImage(rect, self.q_image, rect)
+        self._draw_borders(qp, rect) if self.show_borders else qp.drawImage(rect, self.q_image, rect)
         
         if self.show_rag:
             self._draw_rag(qp)
+
+        if self.show_colors:
+            self._draw_colors(qp)
+
+        if self.show_overlay:
+            self._draw_overlay(qp)
 
         if len(self.selected_segments) > 0:
             for segment in self.selected_segments:
@@ -108,6 +121,7 @@ class Viewport(QWidget):
     def set_image(self, image: np.ndarray):
         self.image = image
         self.q_image = QImage(image, image.shape[1], image.shape[0], QImage.Format_RGB888)
+        self.q_image = self.q_image.scaled(self.width(), self.height(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
         self.update()
 
     def _resize_image(self):
@@ -121,6 +135,7 @@ class Viewport(QWidget):
             self.resize_scale = width / self.q_image.width()
 
     def load_image(self, image_path: str):
+        self.reset()
         self.image = io.imread(image_path, plugin='pil')
         self.image = color.gray2rgb(self.image)
         if self.image is None:
@@ -143,26 +158,45 @@ class Viewport(QWidget):
             return
         
         mask = self.segments == segment
-        image = np.zeros( (self.image.shape[0], self.image.shape[1], 4), dtype=np.uint8)
+        image = np.zeros((self.image.shape[0], self.image.shape[1], 4), dtype=np.uint8)
         image[mask] = (255, 0, 0, 128)
 
         q_image = QImage(image, image.shape[1], image.shape[0], QImage.Format_RGBA8888)
-        q_image = q_image.scaled(self.width(), self.height())
-        qp.drawImage(self.rect(), q_image)
+        q_image = q_image.scaled(self.width(), self.height(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        qp.drawImage(rect, q_image, rect)
 
-    def _draw_borders(self, qp):
+    def _draw_borders(self, qp, rect):
         if self.segments is None:
             return
 
         image = segmentation.mark_boundaries(self.image, self.segments, color=(0, 1, 0), mode='inner')
         image = (image * 255).astype(np.uint8)
         q_image = QImage(image, image.shape[1], image.shape[0], QImage.Format_RGB888)
-        q_image = q_image.scaled(self.width(), self.height())
-        qp.drawImage(self.rect(), q_image)
+        q_image = q_image.scaled(self.width(), self.height(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        qp.drawImage(rect, q_image, rect)
 
     def _draw_rag(self, qp):
         if self.rag is None:
             return
+
+    def _draw_colors(self, qp):
+        if self.segments is None:
+            return
+        
+        # Show segments in different colors
+        image = np.zeros((self.image.shape[0], self.image.shape[1], 4), dtype=np.uint8)
+        for i, segment in enumerate(np.unique(self.segments)):
+            mask = self.segments == segment
+            image[mask] = np.random.randint(0, 255, 3).tolist() + [128]
+
+        q_image = QImage(image, image.shape[1], image.shape[0], QImage.Format_RGBA8888)
+        q_image = q_image.scaled(self.width(), self.height(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        qp.drawImage(0, 0, q_image)
+
+    def _draw_overlay(self, qp):
+        overlay = QImage(str(PROJECT_DIRECTORY / "resources" / "images" / "goal.tif"))
+        overlay = overlay.scaled(self.width(), self.height(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        qp.drawImage(0, 0, overlay)
 
     def toggle_borders(self):
         self.show_borders = not self.show_borders
@@ -172,7 +206,16 @@ class Viewport(QWidget):
         self.show_rag = not self.show_rag
         self.update()
     
+    def toggle_colors(self):
+        self.show_colors = not self.show_colors
+        self.update()
+
+    def toggle_overlay(self):
+        self.show_overlay = not self.show_overlay
+        self.update()
+
     def reset_transform(self):
         self.transform.reset()
         self.update()
+
     